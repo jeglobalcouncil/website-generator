ba7a8001731855e5b49f54802ab7296ad4337d5f
joaobraq <joaobraq@gmail.com>
2018-07-17 20:34:48 -0300

    test

--- themes/lyon-theme/source/assets/scripts.js
+++ themes/lyon-theme/source/assets/scripts.js
@@ -35,6 +35,16 @@ function closeModal() {
   modal.style.display = 'none';
 }
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+}
+
 // ------------------- //
 //  Typewriter effect  //
 // ------------------- //
